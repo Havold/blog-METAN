0a25501f7451514d5a0b6272be1804c9594b5696
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2025-03-29 16:40:11 +0700

    Create Login Page.

--- src/app/dashboard/(auth)/login/page.tsx
+++ src/app/dashboard/(auth)/login/page.tsx
@@ -1,13 +1,58 @@
 "use client";
-import Button from "@/components/Button/Button";
-import { signIn } from "next-auth/react";
 import React from "react";
 
+import styles from "./page.module.css";
+import InputField from "@/components/InputField/InputField";
+import Link from "next/link";
+import Button from "@/components/Button/Button";
+
 const Login = () => {
+  const handleLogin = () => {};
   return (
-    <div>
-      <Button onClick={() => signIn("google")}>Sign In with Google</Button>
-      {/* <button onClick={() => signIn("google")}>Sing In with Google</button> */}
+    <div className={styles.container}>
+      <div className={styles.cardContainer}>
+        <h1 className={styles.title}>Login</h1>
+        <form className={styles.form} action="">
+          <InputField required label="Email" name="email" />
+          <InputField
+            required
+            label="Password"
+            name="password"
+            type="password"
+          />
+          <div className={styles.optContainer}>
+            <div className={styles.rememberContainer}>
+              <input
+                className={styles.checkbox}
+                id="agreement"
+                type="checkbox"
+              />
+              <label style={{ cursor: "pointer" }} htmlFor="agreement">
+                Remember me
+              </label>
+            </div>
+
+            <Link href="#">Forgot Password?</Link>
+          </div>
+
+          <Button
+            size="small"
+            width="inherit"
+            bgColor="#4ed6f5"
+            color="white"
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+        </form>
+
+        <span className={styles.subTitle}>
+          Don&apos;t have an account?{" "}
+          <Link className={styles.link} href="/dashboard/register">
+            Register
+          </Link>
+        </span>
+      </div>
     </div>
   );
 };
